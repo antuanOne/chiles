660bc7882b6744c583882098a6ed10fc30c6fe07
antuan.yanez <antuan.yanez@gmail.com>
2016-11-24 01:00:43 -0700

    resolve issues qith files

---
+++ src/com/pojos/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pojos;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente {
+
+    private String nombre;
+    private String nombreCliente;
+    private Direccion direccion;
+    private String contacto;
+    private String telefono;
+    private String email;
+
+}
